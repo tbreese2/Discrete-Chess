eca20f24870c2a6a9bc764ab89579176d72a5660
tylerbreese <tylerbreese@192.168.0.214>
2022-01-09 11:22:07 -0500

    working on setboard function of engineboard class, havn't finished

--- src/main/java/Engine/EngineBoard.java
+++ src/main/java/Engine/EngineBoard.java
@@ -12,16 +12,18 @@ public class EngineBoard {
     
     //basic bitboards
     private long WhitePawns;
+    private long WhiteKnights;
     private long WhiteBishops;
     private long WhiteRooks;
     private long WhiteQueens;
     private long WhiteKing;
     
     private long BlackPawns;
+    private long BlackKnights;
     private long BlackBishops;
     private long BlackRooks;
     private long BlackQueens;
-    private long BlackKind;
+    private long BlackKing;
     
     //combined bitboards
     private long WhitePieces;
@@ -36,16 +38,18 @@ public class EngineBoard {
     public EngineBoard() {
         //initialize all bitboards to be empty
         WhitePawns = 0L;
+        WhiteKnights = 0L;
         WhiteBishops = 0L;
         WhiteRooks = 0L;
         WhiteQueens = 0L;
         WhiteKing = 0L;
     
         BlackPawns = 0L;
+        BlackKnights = 0L;
         BlackBishops = 0L;
         BlackRooks = 0L;
         BlackQueens = 0L;
-        BlackKind = 0L;
+        BlackKing = 0L;
     
         //combined bitboards
         WhitePieces = 0L;
@@ -80,7 +84,71 @@ public class EngineBoard {
         }
        
        //now use array to set every single bitboard
+       for(r = 7; r >= 0; r--) {
+           for(c = 0; c < 8; c++) {
+                String Binary="0000000000000000000000000000000000000000000000000000000000000000";
+                int i = r * 8 + c;
+                String location=Binary.substring(i+1)+"1"+Binary.substring(0, i);
+                switch(tempBoard[r][c]) {
+                    case "P": 
+                        WhitePawns = Long.parseLong(location, 2) | WhitePawns;
+                        break;
+                    case "N": 
+                        WhiteKnights = Long.parseLong(location, 2) | WhiteKnights;
+                        break;
+                    case "B": 
+                        WhiteBishops = Long.parseLong(location, 2) | WhiteBishops;
+                        break;
+                    case "R": 
+                        WhiteRooks = Long.parseLong(location, 2) | WhiteRooks;
+                        break;
+                    case "Q": 
+                        WhiteQueens = Long.parseLong(location, 2) | WhiteQueens;
+                        break;
+                    case "K": 
+                        WhiteKing = Long.parseLong(location, 2) | WhiteKing;
+                        break;
+                    case "p": 
+                        BlackPawns = Long.parseLong(location, 2) | BlackPawns;
+                        break;
+                    case "n": 
+                        BlackKnights = Long.parseLong(location, 2) | BlackKnights;
+                        break;
+                    case "b": 
+                        BlackBishops = Long.parseLong(location, 2) | BlackBishops;
+                        break;
+                    case "r": 
+                        BlackRooks = Long.parseLong(location, 2) | BlackRooks;
+                        break;
+                    case "q": 
+                        BlackQueens = Long.parseLong(location, 2) | BlackQueens;
+                        break;
+                    case "k": 
+                        BlackKing = Long.parseLong(location, 2) | BlackKing;
+                        break;
+               }
+           }
+       }
        
     }
+    
+    //MODIFES: EngineBoard
+    //EFFECTS: sets the combined boards
+    public void setCombinedBoards() {
+        WhitePieces =  WhitePawns
+        | WhiteKnights
+        | WhiteBishops
+        | WhiteRooks
+        | WhiteQueens
+        | WhiteKing;
+        
+        BlackPieces =  BlackPawns
+        | BlackKnights
+        | BlackBishops
+        | BlackRooks
+        | BlackQueens
+        | BlackKing;
+    }
+    
     
 }
