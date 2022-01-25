1a611b178ef513604febcbc5e927d9376054339c
Tyler <Tyler@LAPTOP-VTE772DR>
2022-01-25 13:39:27 -0800

    added excecutable creation tool

--- src/main/java/UCI.java
+++ src/main/java/UCI.java
@@ -23,12 +23,44 @@ public class UCI {
     public void uciInitialization() {
         String nextLine = inputReader.nextLine();
         
-        if(nextLine.equals("uci")) {
-            System.out.println("id name Discrete Chess");
-            System.out.println("id author Tyler B");
+        while(!nextLine.equals("uci")) {
+            nextLine = inputReader.nextLine();
         }
+        System.out.println("id name Discrete Chess");
+        System.out.println("id author Tyler B");
+        sendOptions();
+        mainGameLoop();
     }
     
+    public void mainGameLoop() {
+        String nextLine = inputReader.nextLine();
+        
+        while(!nextLine.equals("quite")) {
+            if(nextLine.equals("isready")){
+               System.out.println("readyok"); 
+            } else if(nextLine.equals("ucinewgame")) {
+                engine.resetBoard();
+            } else if(nextLine.split("\\s+")[0].equals("position")) {
+                String[] args = nextLine.split("\\s+");
+                if(args[1].equals("startpos")) {
+                    engine.resetBoard();
+                } else {
+                    engine.setBoardFen(args[1]);
+                }
+                if(args[2].equals("moves")) {
+                    for(int i = 3; i < args.length; i++) {
+                        engine.doUCIMove(args[i]);
+                    }
+                }
+            }
+            nextLine = inputReader.nextLine();
+        }
+        
+    }
+    
+    public void sendOptions() {
+        //non game loop
+    }
     public static void main(String[] args) {
        UCI EngineCommunicaiton = new UCI();
     }
