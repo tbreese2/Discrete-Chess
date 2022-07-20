0aa966038bbac05611c4cde42806001324f21f99
tyler <tyler@DESKTOP-1V45FAL>
2022-07-19 19:49:45 -0700

    neoroph added

--- src/main/java/Engine/NNUE/NNUEFactory.java
+++ src/main/java/Engine/NNUE/NNUEFactory.java
@@ -4,18 +4,15 @@
  */
 package Engine.NNUE;
 
-/**
- *
- * @author tbreese
- */
+
 public class NNUEFactory {
-    //L_0: Linear 41024
+    //L_0: Linear 41024->256*2
     //C_0: Clipped ReLu of size 256*2
-    //L_1: Linear 256*2
+    //L_1: Linear 256*2->32
     //C_1: Clipped ReLu of size 32
-    //L_2: Linear 32
+    //L_2: Linear 32->32
     //C_2: Clipped ReLu of size 32
-    //L_3: Linear 32
+    //L_3: Linear 32->1
     //C_3: Clipped ReLu of size 32
     //L_4: Linear 1
 }
